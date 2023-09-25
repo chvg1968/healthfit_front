b73442b2d6dd4f2d0aaaa0fdba49a21dc610cebd
chvg1968 <conradovilla.gmail.com>
2023-09-24 23:38:06 -0500

    update publish

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,8 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  outDir: 'dist',
+  build: {
+    outDir: 'dist', // Esta propiedad especifica la carpeta de salida
+  },
   base: '/healthfit_front/'
 })
