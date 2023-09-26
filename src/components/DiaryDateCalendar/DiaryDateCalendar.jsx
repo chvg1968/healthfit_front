4a2d4a6801e6532fb31a030f504a8f15990b222e
Conrado Hernán Villa Gil <conradovilla@gmail.com>
2023-09-26 13:06:10 -0500

    Actualizar DiaryDateCalendar.jsx

--- src/components/DiaryDateCalendar/DiaryDateCalendar.jsx
+++ src/components/DiaryDateCalendar/DiaryDateCalendar.jsx
@@ -1,8 +1,8 @@
 import { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import 'react-datetime/css/react-datetime.css';
-import 'moment/locale/uk';
-import 'moment/locale/ru';
+import 'moment/locale/es';
+import 'moment/locale/en';
 
 import { diarySelectors, updateDate } from '../../redux/app/diaryPerDay';
 import { diaryPerDayOperation } from '../../redux/app/diaryPerDay';
