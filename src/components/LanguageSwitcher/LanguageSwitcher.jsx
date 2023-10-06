4a097a550ec15ac40257f8060b518aa9b471e14b
chvg1968 <conradovilla.gmail.com>
2023-10-06 14:58:00 -0500

    update change db language

--- src/components/LanguageSwitcher/LanguageSwitcher.jsx
+++ src/components/LanguageSwitcher/LanguageSwitcher.jsx
@@ -1,69 +1,74 @@
 import React, { useEffect, useState } from 'react';
 import { useTranslation } from 'react-i18next';
-import { ButtonLan, LanContainer, LanContainerAuth, LanContainerAuthCalculator } from './LanguageStyled';
+import {
+  ButtonLan,
+  LanContainer,
+  LanContainerAuth,
+  LanContainerAuthCalculator,
+} from './LanguageStyled';
 
-
-
-
-
-function LanguageSwitcher({currentPage, page}) 
-{
- 
-
+function LanguageSwitcher({ currentPage, page }) {
   const { i18n } = useTranslation();
- 
   const [buttonEN, setbuttonEN] = useState(null);
   const [buttonES, setbuttonES] = useState(null);
-  
 
-
-  const changeLang = (lng) => {
-    console.log("lng is ", lng)
-
+  const changeLang = async lng => {
+    console.log('lng is ', lng);
     i18n.changeLanguage(lng);
-    
-  };
-  
-
-
-  
-    useEffect(() => {
-      function handleResize() {
-        
-        if (window.innerWidth <= 600) {
-          setbuttonEN('En') ;
-          setbuttonES('Es') ;
-        } else{
-          setbuttonEN('English') ;
-          setbuttonES('Español') ;
-        }
+        try {
+      const response = await fetch(`https://healthfitback-edf77344271d.herokuapp.com/api/v1/products?lang=${lng}`);
+      if (response.ok) {
+        // Cambiar el idioma en el frontend solo si la solicitud fue exitosa
+        i18n.changeLanguage(lng);
+      } else {
+        console.error('Error al cambiar el idioma en el backend.');
       }
-  
-      window.addEventListener('resize', handleResize);
-      handleResize(); // Llama a la función al cargar la página
-  
-      return () => {
-        window.removeEventListener('resize', handleResize); // Limpia el evento al desmontar el componente
-      };
-    }, []);
-  return (
-    currentPage==='authenticated' ? page=== "calcularor"?
-    <LanContainerAuthCalculator id="lang-container">
-      <ButtonLan id='myButton' onClick={() => changeLang('en')}>{buttonEN}</ButtonLan>
-      <ButtonLan onClick={() => changeLang('es')}>{buttonES}</ButtonLan>
-    </LanContainerAuthCalculator>
-    :
-    <LanContainerAuth id="lang-container">
-      <ButtonLan id='myButton' onClick={() => changeLang('en')}>{buttonEN}</ButtonLan>
-      <ButtonLan onClick={() => changeLang('es')}>{buttonES}</ButtonLan>
-    </LanContainerAuth>
-    :
+    } catch (error) {
+      console.error('Error al cambiar el idioma:', error);
+    }
+}
+
+  useEffect(() => {
+    function handleResize() {
+      if (window.innerWidth <= 600) {
+        setbuttonEN('En');
+        setbuttonES('Es');
+      } else {
+        setbuttonEN('English');
+        setbuttonES('Español');
+      }
+    }
+
+    window.addEventListener('resize', handleResize);
+    handleResize(); // Llama a la función al cargar la página
+
+    return () => {
+      window.removeEventListener('resize', handleResize); // Limpia el evento al desmontar el componente
+    };
+  }, []);
+  return currentPage === 'authenticated' ? (
+    page === 'calcularor' ? (
+      <LanContainerAuthCalculator id="lang-container">
+        <ButtonLan id="myButton" onClick={() => changeLang('en')}>
+          {buttonEN}
+        </ButtonLan>
+        <ButtonLan onClick={() => changeLang('es')}>{buttonES}</ButtonLan>
+      </LanContainerAuthCalculator>
+    ) : (
+      <LanContainerAuth id="lang-container">
+        <ButtonLan id="myButton" onClick={() => changeLang('en')}>
+          {buttonEN}
+        </ButtonLan>
+        <ButtonLan onClick={() => changeLang('es')}>{buttonES}</ButtonLan>
+      </LanContainerAuth>
+    )
+  ) : (
     <LanContainer id="lang-container">
-
-    <ButtonLan id='myButton' onClick={() => changeLang('en')}>{buttonEN}</ButtonLan>
-    <ButtonLan onClick={() => changeLang('es')}>{buttonES}</ButtonLan>
-  </LanContainer>
-
+      <ButtonLan id="myButton" onClick={() => changeLang('en')}>
+        {buttonEN}
+      </ButtonLan>
+      <ButtonLan onClick={() => changeLang('es')}>{buttonES}</ButtonLan>
+    </LanContainer>
   );
 }
 
